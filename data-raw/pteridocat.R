8e442dca7dbbfa592a3e6798cfcd3cfc59857f10
Joel Nitta <joelnitta@gmail.com>
2022-05-23 15:43:10 +0900

    Update data
    Should fix #2

--- data-raw/pteridocat.R
+++ data-raw/pteridocat.R
@@ -1,5 +1,10 @@
 # pteridocat.RDS should be copied here from the pteridocat_maker project
+# https://github.com/fernphy/pteridocat_maker
 
-pteridocat <- readRDS("data/pteridocat.RDS")
+fs::file_copy(
+	"../pteridocat_maker/results/pteridocat.RDS", # path to pteridocat_maker
+	"data-raw/pteridocat.RDS")
+
+pteridocat <- readRDS("data-raw/pteridocat.RDS")
 
 usethis::use_data(pteridocat, overwrite = TRUE)
