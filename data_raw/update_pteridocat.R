9fd00a70ebc61a54ecab2ecad8115118367d06ba
joelnitta <joelnitta@gmail.com>
2023-12-30 07:49:53 +0900

    Add validation

--- data_raw/update_pteridocat.R
+++ data_raw/update_pteridocat.R
@@ -5,6 +5,11 @@ library(dwctaxon)
 
 load_all()
 
+dct_options(
+  valid_tax_status = "accepted, ambiguous synonym, provisionally accepted, synonym",
+  check_mapping_parent = FALSE,
+  check_sci_name = FALSE)
+
 pteridocat <-
   pteridocat |>
   # Change D. giganteum (Baker) Ching to accepted from synonym of D. maximum
@@ -12,6 +17,7 @@ pteridocat <-
     scientificName = "Diplazium giganteum (Baker) Ching",
     taxonomicStatus = "accepted",
     nameAccordingTo = "Wei R, Schneider H, Zhang XC (2013) Taxon 62:441-457"
-  )
+  ) |>
+  dct_validate()
 
 usethis::use_data(pteridocat, overwrite = TRUE)
