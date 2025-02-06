2b66cc1dd9a086a7adc433b98508d27904a4699c
joelnitta <joelnitta@gmail.com>
2025-02-06 18:29:53 +0900

    Update taxonomy
    Move Atalopteris to Ctenitis
    Thelypteris pachyrhachis is synonym of Amauropelta pachyrhachis
    Blechnum fraseri  is synonym of Diploblechnum fraseri
    Cephalomanes singaporianum is synonym of Trichomanes singaporianum (ss)

--- data_raw/update_pteridocat.R
+++ data_raw/update_pteridocat.R
@@ -12,9 +12,40 @@ dct_options(
 
 pteridocat_new <-
   pteridocat |>
+  # Regalado et al 2024 only moved A. aspidioides (type of the genus) to
+  # Ctenitis. There are two more Atalopteris species, but whether they are
+  # truly distinct from A. aspidioides is not clear. These lack
+  # names in Ctenitis. Leave them for now.
+  dct_add_row(
+    scientificName = "Ctenitis cubensis C. Sánchez",
+    taxonomicStatus = "accepted",
+    namePublishedIn = "Regalado, L., Lóriga, J., Beck, A. 2024. Plastid data supports the inclusion of the Greater Antillean genus Atalopteris in Ctenitis (Dryopteridaceae: Polypodiales). American Fern Journal 114(4): 289-307."
+  ) |>
   dct_modify_row(
-    scientificName = "Teratophyllum williamsii (Underw.) Holttum",
+    scientificName = "Atalopteris aspidioides (Griseb.) Maxon & C. Chr.",
+    taxonomicStatus = "synonym",
+    acceptedNameUsage = "Ctenitis cubensis C. Sánchez",
+    remap_parent = FALSE
+  ) |>
+  dct_modify_row(
+    scientificName = "Thelypteris pachyrhachis (Kunze ex Mett.) Ching",
+    taxonomicStatus = "synonym",
+    acceptedNameUsage = "Amauropelta pachyrhachis (Kunze ex Mett.) Salino & T. E. Almeida"
+  ) |>
+  dct_modify_row(
+    scientificName = "Blechnum fraseri var. philippense (Christ) Copel.",
+    taxonomicStatus = "synonym",
+    acceptedNameUsage = "Diploblechnum fraseri (A. Cunn.) De Vol"
+  ) |>
+  dct_modify_row(
+    scientificName = "Trichomanes singaporianum (Bosch) Alderw.",
     taxonomicStatus = "accepted"
+  ) |>
+  dct_modify_row(
+    scientificName = "Cephalomanes singaporianum Bosch",
+    taxonomicStatus = "synonym",
+    acceptedNameUsage = "Trichomanes singaporianum (Bosch) Alderw.",
+    remap_parent = FALSE
   ) |>
   dct_validate()
 
