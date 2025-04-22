d77fb3b382a99af1b70f450c01011724248b4533
joelnitta <joelnitta@gmail.com>
2025-04-22 11:42:26 +0900

    Update names for ftol

--- data_raw/update_pteridocat.R
+++ data_raw/update_pteridocat.R
@@ -6,46 +6,35 @@ library(dwctaxon)
 load_all()
 
 dct_options(
-  valid_tax_status = "accepted, ambiguous synonym, provisionally accepted, synonym",
+  valid_tax_status =
+    "accepted, ambiguous synonym, provisionally accepted, synonym",
   check_mapping_parent = FALSE,
-  check_sci_name = FALSE)
+  check_sci_name = FALSE,
+  remap_parent = FALSE)
 
 pteridocat_new <-
   pteridocat |>
-  # Regalado et al 2024 only moved A. aspidioides (type of the genus) to
-  # Ctenitis. There are two more Atalopteris species, but whether they are
-  # truly distinct from A. aspidioides is not clear. These lack
-  # names in Ctenitis. Leave them for now.
+  # Fix spelling
+  dct_modify_row(
+    taxonID = "37Y37",
+    scientificName = "Dryopteris whangshangensis Ching"
+  ) |>
+  dct_modify_row(
+    taxonID = "37XGF",
+    scientificName = "Dryopteris qandoensis Ching"
+  ) |>
+  # Add names
   dct_add_row(
-    scientificName = "Ctenitis cubensis C. Sánchez",
+    scientificName = "Dryopteris × tiantangzhaiensis H.J.Wei & B.Chen",
     taxonomicStatus = "accepted",
-    namePublishedIn = "Regalado, L., Lóriga, J., Beck, A. 2024. Plastid data supports the inclusion of the Greater Antillean genus Atalopteris in Ctenitis (Dryopteridaceae: Polypodiales). American Fern Journal 114(4): 289-307."
+    taxonRank = "species",
+    namePublishedIn = "Taxon 73(1): 136 (2024)."
   ) |>
-  dct_modify_row(
-    scientificName = "Atalopteris aspidioides (Griseb.) Maxon & C. Chr.",
-    taxonomicStatus = "synonym",
-    acceptedNameUsage = "Ctenitis cubensis C. Sánchez",
-    remap_parent = FALSE
-  ) |>
-  dct_modify_row(
-    scientificName = "Thelypteris pachyrhachis (Kunze ex Mett.) Ching",
-    taxonomicStatus = "synonym",
-    acceptedNameUsage = "Amauropelta pachyrhachis (Kunze ex Mett.) Salino & T. E. Almeida"
-  ) |>
-  dct_modify_row(
-    scientificName = "Blechnum fraseri var. philippense (Christ) Copel.",
-    taxonomicStatus = "synonym",
-    acceptedNameUsage = "Diploblechnum fraseri (A. Cunn.) De Vol"
-  ) |>
-  dct_modify_row(
-    scientificName = "Trichomanes singaporianum (Bosch) Alderw.",
-    taxonomicStatus = "accepted"
-  ) |>
-  dct_modify_row(
-    scientificName = "Cephalomanes singaporianum Bosch",
-    taxonomicStatus = "synonym",
-    acceptedNameUsage = "Trichomanes singaporianum (Bosch) Alderw.",
-    remap_parent = FALSE
+  dct_add_row(
+    scientificName = "Diplazium zangnanense R.Wei & M.J.Lian",
+    taxonomicStatus = "accepted",
+    taxonRank = "species",
+    namePublishedIn = "Nordic J. Bot. 2024(12)-e04334: 5 (2024)."
   ) |>
   dct_validate()
 
